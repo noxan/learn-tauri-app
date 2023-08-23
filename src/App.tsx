26defb3d3528ea466fa0f8394fc4ae19d802aeca
Richard <richard@stromer.org>
2023-08-23 15:09:02 +0200

    list files in react using the tauri fs api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import { FileEntry, readDir } from "@tauri-apps/api/fs";
 import "./App.css";
 
 function App() {
+  const [entries, setEntries] = useState<FileEntry[]>([]);
+
+  readDir("/Users/richard/workspace/tauri-app").then((entries) =>
+    setEntries(entries)
+  );
+
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
+
+      {JSON.stringify(entries)}
     </div>
   );
 }
